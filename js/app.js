a09b8d9a6e5f3bd9c37fbb19de549f430bb2265a
Vanza Setia <venusbumi2@gmail.com>
2022-01-22 16:08:11 +0700

    Add initial validation of the form email

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
 (function () {
   "use strict";
-  console.log("Hello World");
+  const heroForm = document.querySelector(".js-heroForm");
+
+  const showAlert = emailInput => {
+    const heroAlert = document.querySelector(".js-heroAlert");
+    const inputClassName = emailInput.classList[0];
+
+    emailInput.classList.add(`${inputClassName}--error`);
+    heroAlert.removeAttribute("hidden");
+  };
+  const validateEmail = e => {
+    const heroInputEmail = document.querySelector(
+      ".js-heroInputEmail"
+    );
+    const email = heroInputEmail.value;
+    const emailValidation =
+      /^(?:[a-z0-9\.]){2,30}@{1}(?:[a-z0-9-]){2,30}\.{1}(?:[a-z0-9]){2,3}(\.?((?:[a-z0-9]){2,3}))?$/;
+    if (!emailValidation.test(email)) {
+      e.preventDefault();
+      showAlert(heroInputEmail);
+    }
+  };
+
+  heroForm.addEventListener("submit", validateEmail);
 })();
