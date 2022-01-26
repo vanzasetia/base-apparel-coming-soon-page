18d25f7eb69cab3b89741be00953f06331f96ff3
Vanza Setia <venusbumi2@gmail.com>
2022-01-26 15:19:54 +0700

    Format the code
    Format CSS, Sass, and JavaScript

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 (function () {
-  "use strict";
-  const heroForm = document.querySelector(".js-heroForm");
-  const heroAlert = document.querySelector(".js-heroAlert");
-  const heroInputEmail = document.querySelector(".js-heroInputEmail");
+  'use strict';
+  const heroForm = document.querySelector('.js-heroForm');
+  const heroAlert = document.querySelector('.js-heroAlert');
+  const heroInputEmail = document.querySelector('.js-heroInputEmail');
   const heroInputEmailClassName = heroInputEmail.classList[0];
 
   const showAlert = () => {
     heroInputEmail.classList.add(`${heroInputEmailClassName}--error`);
-    heroAlert.setAttribute("role", "alert");
-    heroAlert.removeAttribute("hidden");
+    heroAlert.setAttribute('role', 'alert');
+    heroAlert.removeAttribute('hidden');
   };
   const hideAlert = () => {
     heroInputEmail.classList.remove(
       `${heroInputEmailClassName}--error`
     );
-    heroAlert.removeAttribute("role", "alert");
-    heroAlert.setAttribute("hidden", "");
+    heroAlert.removeAttribute('role', 'alert');
+    heroAlert.setAttribute('hidden', '');
   };
   const validateEmail = e => {
     const email = heroInputEmail.value;
     const emailValidation =
-      /^(?:[a-z0-9\.]){2,30}@{1}(?:[a-z0-9-]){2,30}\.{1}(?:[a-z0-9]){2,3}(\.?((?:[a-z0-9]){2,3}))?$/;
+      /^(?:[a-z0-9.]){2,30}@{1}(?:[a-z0-9-]){2,30}\.{1}(?:[a-z0-9]){2,3}(\.?((?:[a-z0-9]){2,3}))?$/;
     if (!emailValidation.test(email)) {
       e.preventDefault();
       showAlert();
@@ -28,5 +28,5 @@
     }
   };
 
-  heroForm.addEventListener("submit", validateEmail);
+  heroForm.addEventListener('submit', validateEmail);
 })();
